56774b4a46217e12b56b4b9bbd850bd61595a198
mulni342 <75677021+mulni342@users.noreply.github.com>
2021-05-11 09:27:31 -0400

    Update Canvas.js

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,12 +1,32 @@
 Class Ctx {
 
+  constructor(canvas) {
+    this.ctx = canvas.getContext("2d");
+  }
   
+  setColor(color) {
+    ctx.fillStyle = color;
+  }
+  
+  createBox(w, h, x, y) {
+    console.log("comming soon!")
+  }
 
 }
 
 Class Canvas {
-  constructor() {
-    this.ctx = new Ctx();
+  constructor(width, height) {
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.ctx = new Ctx(this.canvas);
   }
+  
+   size(width, height) {
+     this.canvas.width = width;
+     this.canvas.height = height;
+   }
+  
 }
 
+module.exports = { Canvas }
